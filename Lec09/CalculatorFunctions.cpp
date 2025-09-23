7866a28d9c4092acdcee704d5bb7003314092a1a
JCEleanor <crcky4826@gmail.com>
2025-09-22 21:35:08 -0600

    remove object files & executables in gitignore

--- Lec09/CalculatorFunctions.cpp
+++ Lec09/CalculatorFunctions.cpp
@@ -8,31 +8,37 @@ using namespace std;
 
 // separate function definitions
 
-double add(const double LHS, const double RHS) {
+double add(const double LHS, const double RHS)
+{
     return LHS + RHS;
 }
 
-double sub(const double LHS, const double RHS) {
+double sub(const double LHS, const double RHS)
+{
     return LHS - RHS;
 }
 
-double mult(const double LHS, const double RHS) {
+double mult(const double LHS, const double RHS)
+{
     return LHS * RHS;
 }
 
-double div(const double LHS, const double RHS) {
+double div(const double LHS, const double RHS)
+{
     return LHS / RHS;
 }
 
-double mod(const double LHS, const double RHS) {
+double mod(const double LHS, const double RHS)
+{
     const double QUOTIENT = div(LHS, RHS);
     const double BALANCE = QUOTIENT - (int)QUOTIENT;
     return mult(BALANCE, RHS);
 }
 
-void print_equation(const double LHS, const char OP, const double RHS, const double RESULT) {
-    cout << setw(10) << LHS    << " " 
-         << setw(5)  << OP     << " " 
-         << setw(10) << RHS    << " = " 
+void print_equation(const double LHS, const char OP, const double RHS, const double RESULT)
+{
+    cout << setw(10) << LHS << " "
+         << setw(5) << OP << " "
+         << setw(10) << RHS << " == "
          << setw(10) << RESULT << endl;
 }
