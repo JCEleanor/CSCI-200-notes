d13c29ed5b2e693c27e46a82d57a4e8bba3a10fb
JCEleanor <crcky4826@gmail.com>
2025-09-29 09:44:22 -0600

    remove comments in L2C

--- sets/set2/L2C/main.cpp
+++ sets/set2/L2C/main.cpp
@@ -34,18 +34,6 @@ using namespace std;
 
 #include "coordinate_conversion.h"
 
-// void polar_to_cartesian(const double radius, const double angle, double *xCoord, double *yCoord)
-// {
-//     *xCoord = radius * cos(angle);
-//     *yCoord = radius * sin(angle);
-// }
-
-// void cartesian_to_polar(double xCoord, double yCoord, double *radius, double *angle)
-// {
-//     *radius = sqrt(xCoord * xCoord + yCoord * yCoord);
-//     *angle = atan2(yCoord, xCoord);
-// }
-
 /**
  * @brief https://cs-courses.mines.edu/csci200/labs/lab2c.html
  * covert Polar to Cartesian or convert Cartesian to Polar
