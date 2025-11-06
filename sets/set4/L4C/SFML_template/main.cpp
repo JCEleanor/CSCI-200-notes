2d6ec000aa068b161715ff9d5a3cfd2ecf891828
JCEleanor <crcky4826@gmail.com>
2025-11-06 09:55:24 -0700

    add L4C

--- sets/set4/L4C/SFML_template/main.cpp
+++ sets/set4/L4C/SFML_template/main.cpp
@@ -3,10 +3,11 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     // create a window
     sf::Vector2u windowSize(640, 640);
-    sf::RenderWindow window( sf::VideoMode( windowSize ), "SFML Test" );
+    sf::RenderWindow window(sf::VideoMode(windowSize), "A House");
 
     /////////////////////////////////////
     // BEGIN ANY FILE LOADING
@@ -17,14 +18,43 @@ int main() {
     /////////////////////////////////////
 
     // while the window is open
-    while( window.isOpen() ) {
+    while (window.isOpen())
+    {
         // clear any existing contents
         window.clear();
 
         /////////////////////////////////////
         // BEGIN DRAWING HERE
 
-        // place any draw commands here to display in the window
+        // House Body
+        sf::RectangleShape houseBody(sf::Vector2f(200, 200));
+        houseBody.setPosition(sf::Vector2f(220.f, 300.f));
+        houseBody.setFillColor(sf::Color(139, 69, 19)); // Brown
+
+        // Roof
+        sf::CircleShape roof(142, 3);
+        roof.setPosition(sf::Vector2f(173.f, 100.f));
+        roof.setFillColor(sf::Color(255, 0, 0)); // Red
+
+        // Door
+        sf::RectangleShape door(sf::Vector2f(50, 80));
+        door.setPosition(sf::Vector2f(295.f, 420.f));
+        door.setFillColor(sf::Color(165, 42, 42)); // Sienna
+
+        // Windows
+        sf::RectangleShape window1(sf::Vector2f(40, 40));
+        window1.setPosition(sf::Vector2f(240.f, 350.f));
+        window1.setFillColor(sf::Color::Cyan);
+
+        sf::RectangleShape window2(sf::Vector2f(40, 40));
+        window2.setPosition(sf::Vector2f(360.f, 350.f));
+        window2.setFillColor(sf::Color::Cyan);
+
+        window.draw(houseBody);
+        window.draw(roof);
+        window.draw(door);
+        window.draw(window1);
+        window.draw(window2);
 
         //  END  DRAWING HERE
         /////////////////////////////////////
@@ -35,9 +65,11 @@ int main() {
         /////////////////////////////////////
         // BEGIN EVENT HANDLING HERE
         // check if any events happened since the last time checked
-        while( const std::optional event = window.pollEvent() ) {
+        while (const std::optional event = window.pollEvent())
+        {
             // if event type corresponds to pressing window X
-            if( event->is<sf::Event::Closed>() ) {
+            if (event->is<sf::Event::Closed>())
+            {
                 // tell the window to close
                 window.close();
             }
