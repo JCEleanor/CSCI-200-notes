ce56adc893231f5be1b3bee1e72e210b4cf75b35
JCEleanor <crcky4826@gmail.com>
2025-11-24 21:56:36 -0700

    A5: define Polygon and Coordinate

--- sets/set5/A5/Coordinate.hpp
+++ sets/set5/A5/Coordinate.hpp
@@ -1,4 +1,13 @@
 #ifndef COORDINATE_HPP
 #define COORDINATE_HPP
 
+struct Coordinate
+{
+public:
+    Coordinate();
+    Coordinate(double x, double y);
+    double x;
+    double y;
+};
+
 #endif // COORDINATE_HPP
--- sets/set5/A5/Polygon.hpp
+++ sets/set5/A5/Polygon.hpp
@@ -1,4 +1,26 @@
 #ifndef POLYGON_HPP
 #define POLYGON_HPP
 
+#include <SFML/Graphics.hpp>
+#include "Coordinate.hpp"
+
+class Polygon
+{
+public:
+    Polygon();
+    virtual ~Polygon();
+    void setColor(sf::Color);
+    void draw(sf::RenderWindow &);
+    // In UML, when a parameter is listed simply as type (e.g., Coordinate), it generally implies pass-by-value
+    void setCoordinate(int index, const Coordinate &coordinate);
+    virtual bool validate() = 0;
+
+private:
+    sf::Color color;
+
+protected:
+    short numVertices;
+    Coordinate *vertices;
+};
+
 #endif // POLYGON_HPP
