95b0211a48f67a0cf9600c9203827bfacb4e6223
JCEleanor <crcky4826@gmail.com>
2025-12-01 22:15:12 -0700

    L6C: read file and get maze composition

--- sets/set6/L6C/main.cpp
+++ sets/set6/L6C/main.cpp
@@ -1,8 +1,67 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
 
 using namespace std;
-int main()
+int main(int argc, char *argv[])
 {
-    cout << "test";
+
+    string filename;
+
+    if (argc == 1)
+    {
+        cout << "Enter filename for maze: ";
+        cin >> filename;
+    }
+    else
+    {
+        filename = argv[1];
+    }
+
+    fstream fin("./mazePack/" + filename);
+
+    if (fin.fail())
+    {
+        // use a try catch block?
+        cout << "Failed to open file" << endl;
+        return -1;
+    }
+
+    int rowCount, colCount, startRow = -1, startCol = -1;
+    fin >> rowCount >> colCount;
+
+    vector<string> maze;
+    for (int i = 0; i < rowCount; i++)
+    {
+        string row;
+        getline(fin, row);
+        maze.push_back(row);
+
+        // search for starting point
+        for (int j = 0; j < colCount; j++)
+        {
+            if (row[static_cast<size_t>(j)] == 'S')
+            {
+                startCol = j;
+                startRow = i;
+                // S only appear once
+                break;
+            }
+        }
+    }
+
+    // print out maze for test
+    for (int i = 0; i < rowCount; i++)
+    {
+        for (int j = 0; j < colCount; j++)
+        {
+            cout << maze[static_cast<size_t>(i)][static_cast<size_t>(j)];
+        }
+        cout << endl;
+    }
+
+    cout << "starting point " << startRow << " " << startCol << endl;
+    fin.close();
     return 0;
 }
