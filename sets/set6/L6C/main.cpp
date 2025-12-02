c107d16bb1452ee3c1347c2eb1d34b046baf90a5
JCEleanor <crcky4826@gmail.com>
2025-12-01 22:51:36 -0700

    L6C: refactor

--- sets/set6/L6C/main.cpp
+++ sets/set6/L6C/main.cpp
@@ -6,9 +6,70 @@
 #include <SFML/Graphics.hpp>
 
 using namespace std;
+
+/**
+ * @brief print maze for testing purpose
+ *
+ * @param rowCount
+ * @param colCount
+ * @param maze
+ */
+void printMaze(const int rowCount, const int colCount, const vector<string> &maze)
+{
+    // print out maze for test
+    for (int i = 0; i < rowCount; i++)
+    {
+        for (int j = 0; j < colCount; j++)
+        {
+            cout << maze[static_cast<size_t>(i)][static_cast<size_t>(j)];
+        }
+        cout << endl;
+    }
+}
+
+/**
+ * @brief draw maze
+ *
+ * @param rowCount
+ * @param colCount
+ * @param maze
+ * @param window
+ */
+void drawMaze(const int rowCount, const int colCount, const vector<string> &maze, sf::RenderWindow &window)
+{
+    for (int i = 0; i < rowCount; i++)
+    {
+        for (int j = 0; j < colCount; j++)
+        {
+            sf::RectangleShape cell(sf::Vector2f(15.0f, 15.0f));
+            cell.setPosition({j * 15.0f, i * 15.0f});
+
+            // set color
+            char mazeChar = maze[static_cast<size_t>(i)][static_cast<size_t>(j)];
+            if (mazeChar == '#')
+            {
+                cell.setFillColor(sf::Color::Black);
+            }
+            else if (mazeChar == '.')
+            {
+                cell.setFillColor(sf::Color::White);
+            }
+            else if (mazeChar == 'S')
+            {
+                cell.setFillColor(sf::Color::Green);
+            }
+            else if (mazeChar == 'E')
+            {
+                cell.setFillColor(sf::Color::Red);
+            }
+
+            window.draw(cell);
+        }
+    }
+}
+
 int main(int argc, char *argv[])
 {
-
     string filename;
 
     if (argc == 1)
@@ -25,7 +86,6 @@ int main(int argc, char *argv[])
 
     if (fin.fail())
     {
-        // use a try catch block?
         cout << "Failed to open file" << endl;
         return -1;
     }
@@ -57,16 +117,6 @@ int main(int argc, char *argv[])
         }
     }
 
-    // print out maze for test
-    // for (int i = 0; i < rowCount; i++)
-    // {
-    //     for (int j = 0; j < colCount; j++)
-    //     {
-    //         cout << maze[static_cast<size_t>(i)][static_cast<size_t>(j)];
-    //     }
-    //     cout << endl;
-    // }
-
     cout << "starting point " << startRow << " " << startCol << endl;
     fin.close();
     // Create an SFML window that has a width of 15*C and height of 15*R
@@ -87,36 +137,7 @@ int main(int argc, char *argv[])
 
         window.clear();
         // step 2: draw
-        for (int i = 0; i < rowCount; i++)
-        {
-            for (int j = 0; j < colCount; j++)
-            {
-                sf::RectangleShape cell(sf::Vector2f(15.0f, 15.0f));
-                cell.setPosition({j * 15.0f, i * 15.0f});
-
-                // set color
-                char mazeChar = maze[static_cast<size_t>(i)][static_cast<size_t>(j)];
-                if (mazeChar == '#')
-                {
-                    cell.setFillColor(sf::Color::Black);
-                }
-                else if (mazeChar == '.')
-                {
-                    cell.setFillColor(sf::Color::White);
-                }
-                else if (mazeChar == 'S')
-                {
-                    cell.setFillColor(sf::Color::Green);
-                }
-                else if (mazeChar == 'E')
-                {
-                    cell.setFillColor(sf::Color::Red);
-                }
-
-                window.draw(cell);
-            }
-        }
-
+        drawMaze(rowCount, colCount, maze, window);
         window.display();
     }
 
